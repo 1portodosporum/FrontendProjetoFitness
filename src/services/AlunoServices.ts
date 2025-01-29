af2de26dac721c73a5c8833903bfe085610d5939
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-29 12:01:32 -0300

    :sparkles: feat: Implement AlunoServices for managing student data

---
+++ src/services/AlunoServices.ts
@@ -0,0 +1,27 @@
+import Aluno from "../models/Alunos";
+import baseApi from "./baseApi";
+
+class AlunoServices{
+
+    buscarAlunos = async(setDados: Function, header: Object) => {
+        const resposta = await baseApi.get("/alunos", header);
+        setDados(resposta.data);
+    }
+
+    cadastrarAluno = async(aluno: Aluno, setDados: Function , header: Object) => {
+        const resposta = await baseApi.post("/alunos/imc", aluno, header);
+        setDados(resposta.data);
+    }
+
+    atualizarAluno = async(aluno: Aluno, setDados: Function, header: Object) => {
+        const resposta = await baseApi.put("/alunos", aluno, header);
+        setDados(resposta.data);
+    }
+
+    deletarAluno = async(id: string, header: Object) => {
+        await baseApi.delete(`/alunos/${id}`, header);
+    }
+
+}
+
+export default AlunoServices;
