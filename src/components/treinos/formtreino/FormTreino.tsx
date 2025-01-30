d0e1e64071c292f13a8f683aea42beb646d81426
Rute-r <rute.s.r.sousa@gmail.com>
2025-01-30 12:30:29 -0300

    ♻️ refactor formTreino

--- src/components/treinos/formtreino/FormTreino.tsx
+++ src/components/treinos/formtreino/FormTreino.tsx
@@ -7,7 +7,6 @@ import { AuthContext } from '../../../contexts/AuthContext';
 import { RotatingLines } from 'react-loader-spinner';
 
 function FormTreino() {
-
   const navigate = useNavigate();
   const treinoServices = new TreinoServices();
 
@@ -27,7 +26,7 @@ function FormTreino() {
         handleLogout();
       }
     }
-  }
+  };
 
   useEffect(() => {
     if (token === '') {
@@ -45,13 +44,13 @@ function FormTreino() {
   const atualizarEstado = (e: ChangeEvent<HTMLInputElement>) => {
     setTreino({
       ...treino,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
-  }
+  };
 
   const retornar = () => {
     navigate('/listartreinos');
-  }
+  };
 
   const cadastrarTreino = async (e: ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -59,8 +58,7 @@ function FormTreino() {
 
     if (id !== undefined) {
       try {
-
-        await treinoServices.atualizarTreino(treino, setTreino, { headers: { 'Authorization': token } });
+        await treinoServices.atualizarTreino(treino, setTreino, { headers: { Authorization: token } });
         alert('Treino atualizado com sucesso!');
       } catch (error: any) {
         if (error.toString().includes('403')) {
@@ -71,7 +69,7 @@ function FormTreino() {
       }
     } else {
       try {
-        await treinoServices.cadastrarTreino(treino, setTreino, { headers: { 'Authorization': token } });
+        await treinoServices.cadastrarTreino(treino, setTreino, { headers: { Authorization: token } });
         alert('Treino cadastrado com sucesso!');
       } catch (error: any) {
         if (error.toString().includes('403')) {
@@ -83,92 +81,100 @@ function FormTreino() {
     }
     setIsLoading(false);
     retornar();
-  }
-
-
-
-
+  };
 
   return (
-    <section className='bg-white shadow-md rounded-lg p-6 space-y-6'>
-      <form className="space-y-6" onSubmit={cadastrarTreino}>
-        <div className="space-y-2">
-          <label htmlFor="tipo" className="text-sm font-medium text-gray-700 block">
-            Tipo de Treino
-          </label>
-          <div className="relative">
-            <Dumbbell className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
-            <input
-              id="tipo"
-              name="tipo"
-              type="text"
-              className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-red-950 focus:border-red-950 outline-none transition-colors"
-              placeholder="Digite o tipo de treino"
-              value={treino.tipo}
-              onChange={atualizarEstado}
-              required
-            />
+    <div className="min-h-screen bg-gradient-to-br  from-gray-950 to-red-950 flex items-center justify-center p-4">
+      <div className="bg-white w-full max-w-md rounded-2xl shadow-xl p-8">
+        <div className="text-center mb-8">
+          <div className="flex justify-center mb-4">
+            <Dumbbell className="h-12 w-12 text-red-950" />
           </div>
+          <h2 className="text-3xl font-bold text-gray-800">Cadastro de Treino</h2>
+          <p className="text-gray-600 mt-2">Configure o treino</p>
         </div>
-        <div className="grid grid-cols-2 gap-4">
+        <form className="space-y-6" onSubmit={cadastrarTreino}>
           <div className="space-y-2">
-            <label htmlFor="periodo" className="text-sm font-medium text-gray-700 block">
-              Período (meses)
+            <label htmlFor="tipo" className="text-sm font-medium text-gray-700 block">
+              Tipo de Treino
             </label>
             <div className="relative">
-              <Clock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
+              <Dumbbell className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
               <input
-                id="periodo"
-                name="periodo"
+                id="tipo"
+                name="tipo"
                 type="text"
-                min="2"
-                max="36"
-                className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2  focus:ring-red-950 focus:border-red-950 outline-none transition-colors"
-                placeholder="Ex: 3 meses"
-                value={treino.periodo}
+                className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-red-950 focus:border-red-950 outline-none transition-colors"
+                placeholder="Digite o tipo de treino"
+                value={treino.tipo}
                 onChange={atualizarEstado}
                 required
               />
             </div>
           </div>
-          <div className="space-y-2">
-            <label htmlFor="nivel" className="text-sm font-medium text-gray-700 block">
-              Nível
-            </label>
-            <div className="relative">
-              <BarChart className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
-              <select
-                id="nivel"
-                name="nivel"
-                className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2  focus:ring-red-950 focus:border-red-950 outline-none transition-colors appearance-none bg-white"
-                value={treino.nivel}
-                onChange={(e) => setTreino({ ...treino, nivel: e.target.value })}
-                required
-              >
-                <option value="">Selecione</option>
-                <option value="iniciante">Iniciante</option>
-                <option value="intermediario">Intermediário</option>
-                <option value="avancado">Avançado</option>
-              </select>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="space-y-2">
+              <label htmlFor="periodo" className="text-sm font-medium text-gray-700 block">
+                Período (meses)
+              </label>
+              <div className="relative">
+                <Clock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
+                <input
+                  id="periodo"
+                  name="periodo"
+                  type="text"
+                  min="2"
+                  max="36"
+                  className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2  focus:ring-red-950 focus:border-red-950 outline-none transition-colors"
+                  placeholder="Ex: 3 meses"
+                  value={treino.periodo}
+                  onChange={atualizarEstado}
+                  required
+                />
+              </div>
+            </div>
+            <div className="space-y-2">
+              <label htmlFor="nivel" className="text-sm font-medium text-gray-700 block">
+                Nível
+              </label>
+              <div className="relative">
+                <BarChart className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
+                <select
+                  id="nivel"
+                  name="nivel"
+                  className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2  focus:ring-red-950 focus:border-red-950 outline-none transition-colors appearance-none bg-white"
+                  value={treino.nivel}
+                  onChange={(e) => setTreino({ ...treino, nivel: e.target.value })}
+                  required>
+                  <option value="">Selecione</option>
+                  <option value="iniciante">Iniciante</option>
+                  <option value="intermediario">Intermediário</option>
+                  <option value="avancado">Avançado</option>
+                </select>
+              </div>
             </div>
           </div>
+          <button
+            type="submit"
+            className="w-full bg-red-950 text-white py-2 px-4 rounded-lg hover:bg-red-900 focus:ring-4 focus:ring-red-950 font-medium transition-colors">
+            {isLoading ? (
+              <RotatingLines
+                strokeColor="white"
+                strokeWidth="5"
+                animationDuration="0.75"
+                width="24"
+                visible={true}
+              />
+            ) : (
+              <span>{id === undefined ? 'Cadastrar' : 'Atualizar'}</span>
+            )}
+          </button>
+        </form>
+        <div className="mt-6 text-center">
+          <p className="text-sm text-gray-500">Todos os campos são obrigatórios</p>
         </div>
-        <button
-          type="submit"
-          className="w-full bg-red-950 text-white py-2 px-4 rounded-lg hover:bg-red-900 focus:ring-4 focus:ring-red-950 font-medium transition-colors">
-          {isLoading ?
-            <RotatingLines
-              strokeColor="white"
-              strokeWidth="5"
-              animationDuration="0.75"
-              width="24"
-              visible={true}
-            /> :
-            <span>{id === undefined ? 'Cadastrar' : 'Atualizar'}</span>
-          }
-        </button>
-      </form>
-    </section>
+      </div>
+    </div>
   );
 }
 
